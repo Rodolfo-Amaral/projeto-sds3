11abcb3de2ac5971e56ad50ea92fe881beabe169
Rodolfo J. C. Amaral <rodolfocornieri@gmail.com>
2021-05-08 16:30:55 -0300

    DataTable integration

--- frontend/src/types/vendas.ts
+++ frontend/src/types/vendas.ts
@@ -1,4 +1,25 @@
-import { type } from "node:os"
+import { Vendedor } from "./vendedores"
+
+export type Vendas = {
+    id: number;
+    visitas: number;
+    negociado: number;
+    qtde: number;
+    data: string;
+    vendedor: Vendedor;
+}
+
+export type PaginasDeVendas = {
+    content?: Vendas[];
+    last: boolean,
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
 
 export type SomaVenda = {
     nomeVendedor: string;
